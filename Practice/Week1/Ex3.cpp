e4ec7e644539f69943b38369d7f7ccd7dc39ef2e
Elizabeth <beeetiiis@Gmail.com>
2020-10-16 16:28:13 +0300

    Create Ex3.cpp

---
+++ Practice/Week1/Ex3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//using namespace std;
+
+int main() {
+    int n, m;
+    char output;
+    std::cout << "Enter a num between 1 and 26 and a random whole number:\n";
+    std::cin >> n ;
+    std::cin >> m;
+    if (n>=1 && n<=26 && m%2==0) 
+    {
+        output = int('a') + (n-1);
+        std::cout << "Number " << n << " is the letter: " << output;
+    }
+    else if (n>=1 && n<=26 && m%2!=0) 
+    {
+        output = int('A') + (n-1);
+        std::cout << "Number " << n << " is the letter: " << output;
+    }
+    else 
+    {
+        std::cout << "Try again with a num between 1 and 26!";
+    }
+    return 0;
+}
