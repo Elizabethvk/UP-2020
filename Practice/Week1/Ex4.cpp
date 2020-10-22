37365e6f213a942558512ab6a5fbcc7a5158c1b3
Elizabeth <beeetiiis@Gmail.com>
2020-10-23 00:11:45 +0300

    Update Ex4.cpp

--- Practice/Week1/Ex4.cpp
+++ Practice/Week1/Ex4.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+int main() {
+    int n;
+    char input;
+    std::cin >> input >> n;
+    
+    if ( (input >='a' && input<='z') || (input >= 'A' && input <= 'Z') ) {
+        n%=26;
+        if (input >='a') {
+            input = 'a' + (int(input)-'a'+n)%26;
+        }
+        else {
+            input = 'A' + (int(input)-'A'+n)%26;
+        }
+        std::cout<<input;
+    }
+    else {
+        std::cout<<"Error\n";
+    }
+    return 0;
+}
