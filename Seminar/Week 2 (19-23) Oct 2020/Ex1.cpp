69a60cf3d0ce27c5e65f849c7c39504ae84e5ba4
Elizabeth <beeetiiis@Gmail.com>
2020-10-24 00:12:12 +0300

    Create Ex1.cpp

---
+++ Seminar/Week 2 (19-23) Oct 2020/Ex1.cpp
@@ -0,0 +1,22 @@
+/* 1) N числа, изведи най-голямото
+2) сумата и произведението им
+3)   */
+
+#include <iostream>
+
+int main(){
+    int n[5];
+    int maxNum;
+    int number;
+    
+    std::cin >> number;
+    maxNum = n[0];
+    for ()
+   for (int i=0; i<5; i++) {
+       if (maxNum < n[i])
+       {
+           maxNum = n[i];
+       }
+   }
+   std::cout << maxNum;
+}
