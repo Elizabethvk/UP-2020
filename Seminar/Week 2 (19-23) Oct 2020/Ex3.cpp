9034c76ceb8bf934e0950cc36aeafe4d3f87c08e
Elizabeth <beeetiiis@Gmail.com>
2020-10-25 02:17:58 +0300

    Create Ex3.cpp

---
+++ Seminar/Week 2 (19-23) Oct 2020/Ex3.cpp
@@ -0,0 +1,27 @@
+/* 1) N числа, изведи най-голямото
+2) сумата и произведението им
+3)  ver 3  */
+
+#include <iostream>
+
+int main(){
+    int maxNum;
+    int m, tmp;
+
+    std::cin>> m;
+    std::cin >> tmp;
+    maxNum = tmp;
+
+    for (int i=0; i<m; ++i) {
+
+        std::cin>> tmp;
+        if (maxNum < tmp) {
+            maxNum = tmp;
+        }
+        
+    }
+
+    std::cout<< maxNum << std::endl;
+
+   return 0;
+}
