a482ac44bc3da36371772b9ff68b820595c1c15a
Elizabeth <beeetiiis@Gmail.com>
2020-10-30 15:29:26 +0200

    Create Ex1.cpp

---
+++ Practice/Week 2/Ex1.cpp
@@ -0,0 +1,24 @@
+/* Зад. 1*/
+#include <iostream>
+
+int main(){
+    int k, l, ml, bottles;
+    
+    std::cout << "Брой ученици: \n" 
+    std::cin >> k;
+    std::cout << "Брой на литри: \n";
+    std::cin >> l;
+    std::cout  << "Брой на милилитри: \n";
+    std::cin >> ml;
+
+    bottles = (k*400) 
+    /*if (k >= 1 && k <= 26) {
+        if (l >= 1 && l <= 20){
+            if (ml >= 0 && ml < 1000) {
+                
+            }
+        }
+    }*/
+    
+    return 0;
+}
