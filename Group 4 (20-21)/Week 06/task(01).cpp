85a371d8843e74fa5376696b796114b555400787
Elizabeth <beeetiiis@Gmail.com>
2020-11-28 15:45:16 +0200

    Create task(01).cpp

---
+++ Group 4 (20-21)/Week 06/task(01).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    const size_t maxSize = 300;
+    int n[maxSize] = { 0 };
+    int n;
+    std::cin >> n;
+    return 0;
+}
+void writeArrray(int a[], size_t size) {
+    for (int i = 0; i < size; i++) {
+        std::cin >> a[i];
+    }
+}
