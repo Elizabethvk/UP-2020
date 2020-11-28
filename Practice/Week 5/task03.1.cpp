48271618084d1ce38f602011e864522cfb2218e8
Elizabeth <beeetiiis@Gmail.com>
2020-11-28 22:01:29 +0200

    Create task03.1.cpp

---
+++ Practice/Week 5/task03.1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    const unsigned max_size = 500;
+    int a[max_size] = { 0 };
+    int b[max_size] = { 0 };
+    int c[max_size] = { 0 };
+    int n = 0, cnt = 0;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> b[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i];
+        c[n + i] = b[i];
+    }
+    
+    for (int i = 0; i < (2 * n -1); i++) {
+        for (int j = i+1; j < 2 * n; j++) {
+            if (c[i] > c[j]) {
+                c[i] += c[j];
+                c[j] = c[i] - c[j];
+                c[i] -= c[j];
+            }
+        }
+    }
+
+    for (int i = 0; i <2*n; i++) {
+        if (c[i] != c[i + 1]) {
+            std::cout << c[i] << " ";
+        }
+    }
+    return 0;
+}
